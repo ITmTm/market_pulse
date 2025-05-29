4ed60afaab5df8be7f216d4b7c667e9d63690f77
mtm <Stafr95@mail.ru>
2025-05-29 12:07:15 +0500

    slider connection

--- app/js/swiper-init.js
+++ app/js/swiper-init.js
@@ -1,22 +1,36 @@
-let introSwiper = null;
+// let introSwiper = null;
+//
+// function initIntroSwiper() {
+//     const isMobile = window.innerWidth < 768;
+//     if (isMobile && !introSwiper) {
+//         introSwiper = new Swiper('.intro__cards.swiper', {
+//             pagination: {
+//                 el: '.intro__cards .swiper-pagination',
+//                 clickable: true,
+//             },
+//             slidesPerView: 1.2,
+//             spaceBetween: 16,
+//         });
+//     }
+//     if (!isMobile && introSwiper) {
+//         introSwiper.destroy(true, true);
+//         introSwiper = null;
+//     }
+// }
+//
+// window.addEventListener('load', initIntroSwiper);
+// window.addEventListener('resize', initIntroSwiper);
 
-function initIntroSwiper() {
-    const isMobile = window.innerWidth < 768;
-    if (isMobile && !introSwiper) {
-        introSwiper = new Swiper('.intro__cards.swiper', {
-            pagination: {
-                el: '.intro__cards .swiper-pagination',
-                clickable: true,
-            },
-            slidesPerView: 1.2,
-            spaceBetween: 16,
-        });
-    }
-    if (!isMobile && introSwiper) {
-        introSwiper.destroy(true, true);
-        introSwiper = null;
-    }
-}
+const swiper = new Swiper('.swiper.intro__cards', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
 
-window.addEventListener('load', initIntroSwiper);
-window.addEventListener('resize', initIntroSwiper);
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination.intro__cards',
+        clickable: true,
+    },
+});
