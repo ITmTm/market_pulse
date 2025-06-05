9b78c1478b8429f123e1c6124a1a6f75dfc9c2e0
mtm <Stafr95@mail.ru>
2025-06-05 12:52:51 +0500

    swiper implementation

--- app/js/swiper-init.js
+++ app/js/swiper-init.js
@@ -1,47 +1,25 @@
-// let introSwiper = null;
-//
-// function initIntroSwiper() {
-//     const isMobile = window.innerWidth < 768;
-//     if (isMobile && !introSwiper) {
-//         introSwiper = new Swiper('.intro__cards.swiper', {
-//             pagination: {
-//                 el: '.intro__cards .swiper-pagination',
-//                 clickable: true,
-//             },
-//             slidesPerView: 1.2,
-//             spaceBetween: 16,
-//         });
-//     }
-//     if (!isMobile && introSwiper) {
-//         introSwiper.destroy(true, true);
-//         introSwiper = null;
-//     }
-// }
-//
-// window.addEventListener('load', initIntroSwiper);
-// window.addEventListener('resize', initIntroSwiper);
-
-
-
-
-// const swiperElement = document.querySelector(".swiper");
-// const swiperWrapper = swiperElement.querySelector(".swiper-wrapper");
-// const swiperSlides = swiperElement.querySelectorAll(".swiper-slide");
-// swiperSlides.forEach((slide,_) => {
-//     const clonedSlide = slide.cloneNode(true);
-//     swiperWrapper.appendChild(clonedSlide);
-// });
-
 const swiper = new Swiper('.swiper.intro__cards', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    slidesPerView: 4, // 'auto'
+    // mobile
+    slidesPerView: 1, // 'auto'
     spaceBetween: 20,
+    loop: true,
 
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination.intro__cards',
+        clickable: true,
+    },
+
+    breakpoints: {
+        992: {
+            /*  десктоп  */
+            enabled: false,        // ← главный переключатель!
+            slidesPerView: 4,
+            loop: false,
+            allowTouchMove: false,
+            keyboard: { enabled: false },
+            a11y: { enabled: false },
+        },
     },
 });
