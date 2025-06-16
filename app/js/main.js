b0cb0f8f9aa84887f5072f87c482641aaed507db
mtm <Stafr95@mail.ru>
2025-06-16 11:40:47 +0500

    motion effect for bg

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
+// Бургер
 document.querySelector('svg')
     .addEventListener('click', function () {
       this.classList.toggle('active')
     });
+
+
+
+// Вращение заднего фона страницы
+const bg = document.querySelector('.main-bg');
+let latestScrollY = 0;
+let ticking = false;
+
+window.addEventListener('scroll', () => {
+  latestScrollY = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      const angle = latestScrollY * 0.08;
+      bg.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
