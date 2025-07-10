ca01ca9a3de12f21e89387a2c3416c75c9d1bd5f
mtm <Stafr95@mail.ru>
2025-07-10 19:37:13 +0500

    сhanging indents

--- app/js/accordion.js
+++ app/js/accordion.js
@@ -2,7 +2,7 @@
 
 // $(document).ready(function () {
 //     $('.faq__list > li > .answer').hide();
-
+//
 //     $('.faq__list > li').click(function () {
 //         if ($(this).hasClass("active")) {
 //             $(this).removeClass("active").find(".answer").slideUp();
@@ -17,8 +17,8 @@
 
 // новая версия аккордеона
 $(document).ready(function () {
-    $('.partner-tags__list > li').click(function (e) {
-        $(".partner-tags__list > li").not($(this)).removeClass("active").find(".answer").slideUp();
-        $(this).toggleClass("active").find(".answer").slideToggle();
-    });
+  $('.partner-tags__list > li').click(function (e) {
+    $(".partner-tags__list > li").not($(this)).removeClass("active").find(".answer").slideUp();
+    $(this).toggleClass("active").find(".answer").slideToggle();
+  });
 });
