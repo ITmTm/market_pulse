7e9cdb8ade18d1efd8044ceaffdaf1d5fc33e9a5
mtm <Stafr95@mail.ru>
2025-07-15 21:27:40 +0500

    сhanging indents

--- app/js/table.js
+++ app/js/table.js
@@ -8,32 +8,32 @@ let tableTr = $(".start-instruments__table tr");
 tableHide.css('display', 'none');
 
 for (let i = 0; i < tableBtn.length; i++) {
-    tableBtn.eq(i).on("click", () => {
-        for (let n = 0; n < tableBtn.length; n++) {
-            tableBtn.eq(n).removeClass("active");
-            tableTable.eq(n).removeClass("active");
-        };
+  tableBtn.eq(i).on("click", () => {
+    for (let n = 0; n < tableBtn.length; n++) {
+      tableBtn.eq(n).removeClass("active");
+      tableTable.eq(n).removeClass("active");
+    }
 
-        tableBtn.eq(i).addClass("active");
-        tableTable.eq(i).addClass("active");
-        hideTr();
-    });
-};
+    tableBtn.eq(i).addClass("active");
+    tableTable.eq(i).addClass("active");
+    hideTr();
+  });
+}
 
 function showTr() {
-    tableTr.addClass('active');
-    tableShow.css('display', 'none');
-    tableHide.css('display', 'flex');
+  tableTr.addClass('active');
+  tableShow.css('display', 'none');
+  tableHide.css('display', 'flex');
 }
 
 function hideTr() {
-    tableTr.removeClass('active');
-    tableHide.css('display', 'none');
-    tableShow.css('display', 'flex');
+  tableTr.removeClass('active');
+  tableHide.css('display', 'none');
+  tableShow.css('display', 'flex');
 
-    $("body, html").animate({
-        scrollTop: $('.start-instruments').offset().top
-    }, 600);
+  $("body, html").animate({
+    scrollTop: $('.start-instruments').offset().top
+  }, 600);
 }
 
 tableShow.on('click', showTr);
